08cf57c5b708a3fb2f7cce8777f901fd7fb4eb40
vinaykuchi <vinaykuchi321@gmail.com>
2024-02-16 15:50:41 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        vinay:<input type="text" name="vinay" value=" "/>
+        password:<input type="text" name="vinay" value=" "/>
+        <input type="submit" name="submit"/>
+    </form>
+    </div>
+  );
+}
+
+export default App;
